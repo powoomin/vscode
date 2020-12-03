f587e1e32fc6cb2f5aff09f16f5e68280fe242b9
MW_Lee <21900505@handong.edu>
2020-12-03 12:27:35 +0900

    첫날커밋

---
+++ baekjoon/1.입출력과사칙연산/2.we_love_kriii_줄바꿈출력/c++_we_love_krii.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    std::cout<<"we love you :D"<<std::endl;
+    std::cout<<"we love me :D"<<std::endl;
+}
---
+++ baekjoon/1.입출력과사칙연산/2.we_love_kriii_줄바꿈출력/java_we_love_kriii.java
@@ -0,0 +1,8 @@
+public class java_we_love_kriii
+{
+    public static void main(String[] args)
+    {
+        System.out.println("we love you :D");
+        System.out.println("we love me :D");
+    }
+}
