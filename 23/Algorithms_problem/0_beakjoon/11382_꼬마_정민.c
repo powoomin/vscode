759711ad9d85d14142f4d1253153bacbf45f64dc
powoomin <wooane21@gmail.com>
2023-07-10 02:12:08 +0900

    BJ

--- 23/Algorithms_problem/0_beakjoon/11382_꼬마_정민.c
+++ 23/Algorithms_problem/0_beakjoon/11382_꼬마_정민.c
@@ -1,12 +1,16 @@
 //https://www.acmicpc.net/problem/11382
 #include <stdio.h>
-
 //첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 10^12)이 공백을 사이에 두고 주어진다.
-
 //A+B+C의 값을 출력한다.
 
+long long Input_and_Sum();
+void Print(long long sum);
 
-
+int main(void)
+{
+    long long sum = Input_and_Sum();
+    Print(sum);
+}
 
 long long Input_and_Sum()
 {
@@ -22,8 +26,3 @@ void Print(long long sum)
     printf("%lld", sum);
 }
 
-int main(void)
-{
-    long long sum = Input_and_Sum();
-    Print(sum);
-}
