1b8995bb158dabe16434e620b58805bbb8d9ec9a
powoomin <wooane21@gmail.com>
2024-02-21 17:56:09 +0800

    2/21오랜만잔디

---
+++ 24/t.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+void print_star() // 2/21 그냥 별 찍기
+{
+    printf("Hellow World :D\n");
+
+    printf("\nprint : star\n");
+    for(int i=0; i<5; i++) //단순 별
+    {
+        for(int j=0; j<i+1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("\ntree star\n");
+    for(int i=0; i<10; i++) //별나무
+    {
+        for(int j=10; j>i+1; j--)
+        {
+            printf(" ");
+        }
+        for(int j=0; j<i+1; j++)
+        {
+            printf("*");
+        }
+        for(int j=0; j<i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("\nempty_tree star\n"); //무늬 별나무
+    for(int i=0; i<10; i++)
+    {
+        for(int j=10; j>i+1; j--)
+        {
+            printf(" ");
+        }
+        printf("*");
+        for(int j=0; j<i; j++)
+        {
+            if(i%2==0) printf(" ");
+            else printf("*");
+        }
+        for(int j=0; j<i-1; j++)
+        {
+            if(i%2==1) printf(" ");
+            else printf("*");
+        }
+        if(i==0) printf("\n");
+        else printf("*\n");
+
+
+    }
+}
+
+int main(void)
+{
+    print_star();
+    return 0;
+}
+
